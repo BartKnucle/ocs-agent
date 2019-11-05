50b476588b3dc627bb78cb9c1fd77bee108d6ab7
BartKnucle <sjarde@yahoo.fr>
2019-11-05 13:57:55 +0100

    Clear sandox

--- pages/sandbox.vue
+++ pages/sandbox.vue
@@ -1,73 +1,17 @@
 <template>
   <section>
-    <v-card>
-      <v-system-bar>
-        Hostname: {{ hostname.data }}
-      </v-system-bar>
-      <table>
-        <tr v-for="(item, key) in sys" :key="key">
-          <td>
-            {{ item._id }}
-          </td>
-          <td>
-            {{ item.data }}
-          </td>
-        </tr>
-      </table>
-      <v-card>
-        <v-system-bar>
-          OS: {{ distro }}
-        </v-system-bar>
-        <table>
-          <tr v-for="(item, key) in os" :key="key">
-            <td>
-              {{ item._id }}
-            </td>
-            <td>
-              {{ item.data }}
-            </td>
-          </tr>
-        </table>
-      </v-card>
-    </v-card>
+    Sandbox
   </section>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
-
 export default {
-  name: 'System',
+  name: 'Sandbox',
   data () {
-    return {
-      hostname: {
-        _id: '',
-        data: ''
-      },
-      distro: {
-        _id: '',
-        data: ''
-      }
-    }
-  },
-  computed: { // only getters have live queries
-    ...mapGetters('system', { system: 'find', get: 'get' }),
-    sys () {
-      return this.system().data.filter(x => x._id.startsWith('sys.') === true)
-    },
-    os () {
-      return this.system().data.filter(x => x._id.startsWith('os.') === true)
-    }
+    return {}
   },
   mounted () {
-    this.find()
-      .then(() => {
-        this.hostname = this.get('os.hostname')
-        this.distro = this.get('os.distro')
-      })
-  },
-  methods: {
-    ...mapActions('system', { find: 'find' })
+    console.log(Navigator.credentials)
   }
 }
 </script>
