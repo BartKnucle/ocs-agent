956d218a62a0ce10f631b0050230c10c0d62d4cb
BartKnucle <sjarde@yahoo.fr>
2019-11-21 14:54:44 +0100

    Load initial data

--- server/services/interfaces/index.js
+++ server/services/interfaces/index.js
@@ -17,63 +17,64 @@ class Interfaces extends Service {
   fill () {
     super.fill()
 
-    //  Get data from database
-    this.service.find()
-      .then((dbData) => {
-        // Get new data
-        si.networkInterfaces()
-          .then((interfaces) => {
-            //  Remove interfaces not new data
-            dbData.forEach((iface) => {
-              if (!interfaces.find(x => x.iface === iface._id)) {
-                delete this.data[iface._id]
+    si.networkInterfaces()
+      .then((interfaces) => {
+        //  Remove interfaces not new data
+        for (const iface in this.data) {
+          if (!interfaces.find(x => x.iface === iface)) {
+            delete this.data[iface]
+          }
+        }
+
+        // Get the os library interfaces data
+        const osNetworkInterfaces = os.networkInterfaces()
+
+        //  Get the default gateway
+        defaultGateway.v4()
+          .then((defaultIface) => {
+            //  Update data
+            interfaces.forEach((iface) => {
+              //  filter interface fields
+              const { duplex, speed, mtu, carrierChanges, ...rest } = iface
+
+              //  Add the os library informations
+              const osInterfaceV4 = osNetworkInterfaces[iface.iface].find(x => x.address === iface.ip4)
+              iface.ip4_subnet = osInterfaceV4.cidr
+
+              const osInterfaceV6 = osNetworkInterfaces[iface.iface].find(x => x.address === iface.ip6)
+              iface.ip6_subnet = osInterfaceV6.cidr
+
+              // Check if this it the default interface
+              if (rest.iface === defaultIface.interface) {
+                rest.default = true
+                // Update device IP data
+                this.app.device.data.ip4 = iface.ip4
+                this.app.device.data.ip4_subnet = iface.ip4_subnet
+                this.app.device.data.gatewayV4 = defaultIface.gateway
+                // Get the default gateway
               }
+
+              this.data[iface.iface] = rest
             })
-
-            // Get the os library interfaces data
-            const osNetworkInterfaces = os.networkInterfaces()
-
-            //  Get the default gateway
-            defaultGateway.v4()
-              .then((defaultIface) => {
-                //  Update data
-                interfaces.forEach((iface) => {
-                  //  filter interface fields
-                  const { duplex, speed, mtu, carrierChanges, ...rest } = iface
-
-                  //  Add the os library informations
-                  const osInterfaceV4 = osNetworkInterfaces[iface.iface].find(x => x.address === iface.ip4)
-                  iface.ip4_subnet = osInterfaceV4.cidr
-
-                  const osInterfaceV6 = osNetworkInterfaces[iface.iface].find(x => x.address === iface.ip6)
-                  iface.ip6_subnet = osInterfaceV6.cidr
-
-                  // Check if this it the default interface
-                  if (rest.iface === defaultIface.interface) {
-                    rest.default = true
-                    // Update device IP data
-                    this.app.device.data.ip4 = iface.ip4
-                    this.app.device.data.ip4_subnet = iface.ip4_subnet
-                    this.app.device.data.gatewayV4 = defaultIface.gateway
-                    // Get the default gateway
-                  }
-
-                  this.data[iface.iface] = rest
-                })
-              })
-              .catch((err) => {
-                this.log({
-                  level: 2,
-                  text: `Cannot default interface: ${err}`
-                })
-              })
           })
           .catch((err) => {
             this.log({
               level: 2,
-              text: `Cannot network interfaces: ${err}`
+              text: `Cannot default interface: ${err}`
             })
           })
+      })
+      .catch((err) => {
+        this.log({
+          level: 2,
+          text: `Cannot network interfaces: ${err}`
+        })
+      })
+
+    //  Get data from database
+    this.service.find()
+      .then((dbData) => {
+        // Get new data
       })
       .catch((err) => {
         this.log({
