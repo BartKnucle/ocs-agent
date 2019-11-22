ce12fa4372c5d80ef8bfdd5c8b27b2908b12ec42
BartKnucle <sjarde@yahoo.fr>
2019-11-22 22:02:49 +0100

    fix data changed

--- server/hooks/dataChanged.js
+++ server/hooks/dataChanged.js
@@ -1,6 +1,9 @@
 const dataChanged = async (context) => {
   const oldData = await context.service.get(context.id)
-  return Object.keys(context.data).reduce((result, item) => {
+  return Object.keys(context.data)
+    .filter(x => x !== '_id')
+    .filter(x => x !== 'updated')
+    .reduce((result, item) => {
     result = (oldData[item] !== context.data[item]) || result
     return result
   }, false)
@@ -9,7 +12,10 @@ const dataChanged = async (context) => {
 module.exports = () => {
   return async (context) => {
     if (await dataChanged(context)) {
+      console.log('change')
       return context
+    } else {
+      context.result = false
     }
   }
 }
