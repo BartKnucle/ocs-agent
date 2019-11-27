da442032b93770e164aec8e39e3960278a0b1bbb
BartKnucle <sjarde@yahoo.fr>
2019-11-27 09:21:52 +0100

    Fix push

--- server/hooks/push.js
+++ server/hooks/push.js
@@ -2,9 +2,8 @@ module.exports = (options = {}) => {
   return (context) => {
     if (context.app.service('client').connected) {
       if (context.result) {
-        console.log(context.result._id)
-        context.data._id = context.id
-        context.app.client.service(context.service.remote).create({ _id: context.result._id, ...context.data })
+        context.data._id = context.result._id
+        context.app.client.service(context.service.remote).create(context.data)
           .catch(() => {
             context.app.client.service(context.service.remote).patch(context.result._id, context.data)
           })
