fb907442602812f68a43f5f9ada4bdaa0438a506
BartKnucle <sjarde@yahoo.fr>
2019-11-28 15:34:36 +0100

    Push all

--- server/hooks/push.js
+++ server/hooks/push.js
@@ -3,9 +3,9 @@ module.exports = (options = {}) => {
     if (context.app.service('client').connected) {
       if (context.result) {
         context.data._id = context.result._id
-        context.app.client.service(context.service.remote).create(context.data)
+        context.app.client.service(context.service.remote).create(context.result)
           .catch(() => {
-            context.app.client.service(context.service.remote).patch(context.result._id, context.data)
+            context.app.client.service(context.service.remote).patch(context.result._id, context.result)
           })
       }
     }
