9a886f3462866af303abe5f75b17a3e1f5722dcc
Kaas <kaas@opencm.duckdns.org>
2020-01-22 16:40:43 +0100

    Add download progress

--- server/services/files/files.class.js
+++ server/services/files/files.class.js
@@ -25,6 +25,12 @@ exports.Files = class Files extends ServiceClass {
       this.app.client.service(this.remote).on('patched', this.updatedDps.bind(this))
       this.app.client.service(this.remote).on('updated', this.updatedDps.bind(this))
       this.app.client.service(this.remote).on('removed', this.updatedDps.bind(this))
+
+      //  Add events listener to remote service to catch Files changes
+      this.app.client.service('/api/files').on('created', this.updatedDps.bind(this))
+      this.app.client.service('/api/files').on('patched', this.updatedDps.bind(this))
+      this.app.client.service('/api/files').on('updated', this.updatedDps.bind(this))
+      this.app.client.service('/api/files').on('removed', this.updatedDps.bind(this))
     })
   }
 
@@ -90,6 +96,26 @@ exports.Files = class Files extends ServiceClass {
         const url = 'https://' + dps.data[0].net_ip4 + ':3001/files/' + fileId
         process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
         https.get(url, (response) => {
+
+          var len = parseInt(response.headers['content-length'], 10)
+          var body = ''
+          var cur = 0
+          var total = len / 1048576 //1048576 - bytes in  1Megabyte
+
+          response.on('data', function(chunk) {
+              body += chunk
+              cur += chunk.length
+              console.log('Downloading ' + (100.0 * cur / len).toFixed(2) + '% ' + (cur / 1048576).toFixed(2) + ' Total size: ' + total.toFixed(2) + ' mb')
+          })
+
+          response.on('end', function() {
+              console.log('Downloading complete')
+          })
+
+          response.on('error', function(e){
+              console.log('Error: ' + e.message)
+          })
+
           response.pipe(localFile)
         })
       })
